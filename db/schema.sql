4e394d8633c7f0442e4e55967a335b4fbb97fab0
Bret Baker <bretbaker808@gmail.com>
2019-09-18 14:03:32 -0700

    boilerplate pushed, will modify tomorrow

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-	id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(255),
-    devoured BOOLEAN,
-    PRIMARY KEY (id)
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `burger_name` VARCHAR(255) NOT NULL,
+    `devoured` BOOLEAN DEFAULT false,
+    PRIMARY KEY (`id`)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO burgers (burger_name, devoured)
-VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name)
+VALUES ('Hamburger');
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name)
+VALUES ('Cheeseburger');
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ('Veggie burger', false); 
+INSERT INTO burgers (burger_name)
+VALUES ('Veggie burger'); 
